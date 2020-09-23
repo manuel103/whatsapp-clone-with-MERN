1409207383e176fdeffffa4fafb35ddfdb3c8d22
Manuel103 <enokwiri@gmail.com>
2020-09-23 10:43:25 +0300

    first commit

---
+++ frontend/src/Chat.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import InsertEmotIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+
+function Chat() {
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen... </p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Emmanuel</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Emmanuel</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name">Emmanuel</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmotIcon />
+        <form>
+          <input type="text" placeholder="Type a message" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ frontend/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdH2Fru5e9B1Z4Keoo3NSFgvMfRW5P8cg",
+  authDomain: "whatsapp-clone-2c4e6.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-2c4e6.firebaseio.com",
+  projectId: "whatsapp-clone-2c4e6",
+  storageBucket: "whatsapp-clone-2c4e6.appspot.com",
+  messagingSenderId: "409131437595",
+  appId: "1:409131437595:web:46ba5efb601795a0e932be",
+};
